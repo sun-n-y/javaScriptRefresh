a107f37b4be4e15b3eb9f187a40a846d553ddff3
sunny patel <sunnykpatel.skp@gmail.com>
2024-03-18 21:31:30 -0400

    add numbers lesson & challenge

---
+++ 06-numbers/app.js
@@ -0,0 +1,12 @@
+const number = 35;
+let pants = 2.456;
+pants = 'pants are great';
+pants = 2;
+
+console.log(number);
+console.log(pants);
+
+const division = number / pants;
+const division2 = number % pants;
+console.log(parseFloat(division.toFixed(2)));
+console.log(division2);
---
+++ 07-numbers-challenge/app.js
@@ -0,0 +1,20 @@
+let score1 = 1;
+let score2 = 2;
+let score3 = 3;
+
+const sumScore = score1 + score2 + score3;
+const averageScore = sumScore / 3;
+
+console.log(sumScore);
+console.log(averageScore);
+
+const plates = 20;
+const people = 7;
+
+let remainingPlates = 20 % 7;
+
+remainingPlates++;
+
+const message = `There are ${remainingPlates} available`;
+
+console.log(message);
