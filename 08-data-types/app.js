c8bf1bc83b3f8d8e974b1974ee3cbd2c0e8b2f56
sunny patel <sunnykpatel.skp@gmail.com>
2024-03-19 20:40:18 -0400

    add arrays lesson and challenge

--- 08-data-types/app.js
+++ 08-data-types/app.js
@@ -3,3 +3,14 @@ const text = 'some text';
 console.log(text);
 console.log(typeof text);
 console.log(typeof true);
+
+const friend1 = 'anna';
+const friend2 = 'tim';
+const friend3 = 'louis';
+const friend4 = 'gene';
+
+const friendsArray = [friend1, friend2, friend3, 'gene', true, 9, null];
+
+console.log(friendsArray);
+console.log(friendsArray[0]);
+console.log(friendsArray[9]);
---
+++ 09-arrays-challenge/app.js
@@ -0,0 +1,8 @@
+const fruits = ['apple', 'banana', 'orange', 10];
+
+let firstFruit = fruits[0];
+
+fruits[3] = firstFruit;
+
+console.log(firstFruit);
+console.log(fruits);
