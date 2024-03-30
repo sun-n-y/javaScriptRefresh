3d0e4dbb6d0f42afd5b7260e9411e324d506b0c6
sunny patel <sunnykpatel.skp@gmail.com>
2024-03-30 13:08:33 -0400

    add event target lessons

--- 22-events/app.js
+++ 22-events/app.js
@@ -1,7 +1,13 @@
 //click event
 
 const btn = document.querySelector('.btn');
+const h1 = document.querySelector('h1');
 
-btn.addEventListener('click', function () {
-  console.log('you cliked button');
+btn.addEventListener('click', function (e) {
+  console.log('you clicked button');
+  console.log(e.currentTarget);
+});
+
+h1.addEventListener('click', function (e) {
+  console.log(e.currentTarget);
 });
