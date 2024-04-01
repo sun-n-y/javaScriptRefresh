f4c516504b4fc3787bc6798d672f4146cd183248
sunny patel <sunnykpatel.skp@gmail.com>
2024-04-01 11:57:18 -0400

    add hoisting lesson

---
+++ 26-hoisting/app.js
@@ -0,0 +1,14 @@
+display();
+display2();
+
+const firstName = 'jordan';
+let lastName = 'kobe';
+var other = 'lebron';
+
+function display() {
+  console.log('hello world');
+}
+
+function display2() {
+  console.log(` ${other}`);
+}
