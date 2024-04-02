4d0cceccf7a373cbbdb47e54cf285439cb450609
sunny patel <sunnykpatel.skp@gmail.com>
2024-04-01 22:12:46 -0400

    add es6 lesson

---
+++ 28-ES6/app.js
@@ -0,0 +1,14 @@
+// //define
+// var number = 100;
+// //update
+// number = 200;
+// //redefine
+// var number = 'orange';
+// console.log(number);
+
+// let amount = 100;
+// amount = 50;
+// let amount = 1000;
+
+// const amount = 100;
+// const amount = 50;
