a3332ceed7e4f666ccd0a84534adc37218a4f636
sunny patel <sunnykpatel.skp@gmail.com>
2024-04-02 11:51:48 -0400

    add destructuring lesson

--- 28-ES6/app.js
+++ 28-ES6/app.js
@@ -12,3 +12,24 @@
 
 // const amount = 100;
 // const amount = 50;
+
+// const lastName = 'timmy';
+
+// const timmy = {
+//   firstName: 'jimmy',
+//   sayName: () => {
+//     console.log(this);
+//     console.log(`${this.lastName}`);
+//   },
+// };
+
+// timmy.sayName();
+
+const fruits = ['orange', 'banana', 'lemon'];
+const fruitsO = { one: 'orange', two: 'banana', three: 'lemon' };
+
+const [orange, six, zero] = fruits;
+const { one, two, three } = fruitsO;
+
+console.log(orange);
+console.log(one);
